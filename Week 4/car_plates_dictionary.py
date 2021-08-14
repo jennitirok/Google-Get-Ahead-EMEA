255377084e3dd9d5358bdfb94ffce037e289275b
Jennifer Gabriela Tirok <jennifer_tirok@yahoo.com>
2021-08-14 16:48:12 +0700

    Week 4: Car Plates Dictionary

---
+++ Week 4/car_plates_dictionary.py
@@ -0,0 +1,33 @@
+def find_shortest_word(plate, vocabulary):
+    """Return the shortest word in the vocabulary with all the letters in a license plate."""
+    word_list = []
+
+    # Filter the 'plate' string, grabing only letters in lowercase (a-z)
+    plate = plate.lower()
+    for i in plate:
+        if (ord(i) in range(97, 123)):
+            word_list.append(i)
+
+    # Iterate through the list of vocabulary and compare the letters in the plate with each vocabulary
+    matching_vocabs = []
+    for vocab in vocabulary:
+        vocab = vocab.lower()
+        matching_counter = 0
+        for char in word_list:
+            if char in vocab:
+                matching_counter += 1
+        matching_vocabs.append(tuple((matching_counter, vocab)))
+
+    # Grab the highest number of matched letters
+    max_counter = max([x[0] for x in matching_vocabs])
+
+    # Filter the vocabulary, grabing only the ones with the highest max_counter
+    final_list = []
+    for pair in matching_vocabs:
+        if (pair[0] == max_counter):
+            final_list.append(pair[1])
+
+    # Return the shortest word matched with the letters in the 'plate' string
+    return min(final_list, key=len)
+
+    
