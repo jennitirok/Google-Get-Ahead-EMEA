3b6ae893ee840ceee5cfb9965940eaab6dbd09cc
Jennifer Gabriela Tirok <jennifer_tirok@yahoo.com>
2021-08-17 14:18:11 +0700

    Week 4: Car Plates Dictionary

--- Week 4/car_plates_dictionary.py
+++ Week 4/car_plates_dictionary.py
@@ -10,16 +10,30 @@ def find_shortest_word(plate, vocabulary):
 
     # Iterate through the list of vocabulary and compare the letters in the plate with each vocabulary
     matching_vocabs = []
+    if vocabulary == []:
+        return ""
+
     for vocab in vocabulary:
         vocab = vocab.lower()
         matching_counter = 0
+        seen = []
         for char in word_list:
             if char in vocab:
-                matching_counter += 1
+                if char in seen and vocab.count(char) == word_list.count(char):
+                    matching_counter += 2
+                else:
+                    matching_counter += 1
+                    seen.append(char)
+            else:
+                matching_counter = 0
+                break
         matching_vocabs.append(tuple((matching_counter, vocab)))
 
     # Grab the highest number of matched letters
     max_counter = max([x[0] for x in matching_vocabs])
+
+    if max_counter == 0:
+        return ""
 
     # Filter the vocabulary, grabing only the ones with the highest max_counter
     final_list = []
