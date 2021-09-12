263c0d77f63416e3cb726d8f3ec4b6ea6fae26f7
Jennifer Gabriela Tirok <jennifer_tirok@yahoo.com>
2021-09-12 12:17:05 +0100

    Week 6: Minesweeper

--- Week 6/minesweeper.py
+++ Week 6/minesweeper.py
@@ -1,7 +1,40 @@
+class Cell:
+
+  def __init__(self):
+    # Initialise all cells in Minefield with 0
+    self.value = 0
+    self.is_visible = False
+
+class Matrix:
+
+  def __init__(self):
+    # Initial Matrix size
+    self.rows = 0
+    self.cols = 0
+    # Will be a two-dimensional array
+    self.matrix = []
+
+  def resize(self, rows, cols):
+    self.rows = rows
+    self.cols = cols
+
+    # Fill each rows and cols with a Cell
+    # e.g. [[0,0], [0,0]] for a 2x2 Matrix
+    row = []
+    for i in range(0, self.rows):
+      row = [Cell() for j in range(0, self.cols)]
+      self.matrix.append(row)
+
 class Minefield:
 
   def __init__(self, rows, cols, number_of_mines):
-    pass
+    self.field = Matrix()
+    self.field.resize(rows, cols)
+
+    # Start assigning each field with its respective number
+    # Check so that number_of_mines do not exceed the size of the field
+    if number_of_mines > (rows * cols):
+      raise ValueError("Number of Mines cannot exceed the size of field")
 
   def on_click(self, row, col):
     pass
